bd51ca92296b05d0e944a4f4312da94a73813ae3
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2023-03-08 20:14:37 +0100

    CEFP postprocessing and checks

---
+++ EventFiltering/macros/cefpOutputChecker.C
@@ -0,0 +1,42 @@
+#include <TFile.h>
+#include <TTree.h>
+
+void cefpOutputChecker(std::string histoFile = "AnalysisResults.root", std::string treeFile = "AO2D.root") {
+  TFile referenceFile(histoFile.data());
+  TH1* refScalers = (TH1*)referenceFile.Get("central-event-filter-task/scalers/mScalers");
+  TH1* refFilters = (TH1*)referenceFile.Get("central-event-filter-task/scalers/mFiltered");
+
+  TFile outputFile("cefpOutputChecker.root", "recreate");
+  TH1* newFilters = (TH1*)refFilters->Clone("newFilters");
+  TH1* newScalers = (TH1*)refScalers->Clone("newScalers");
+  newFilters->Reset();
+  newScalers->Reset();
+
+  TFile inputFile(treeFile.data());
+  for (auto key : *(inputFile.GetListOfKeys())) {
+    TTree* cefpTree = (TTree*)inputFile.Get(Form("%s/O2cefpdecision", key->GetName()));
+    if (!cefpTree) continue;
+    ULong64_t fCefpSelected, fCefpTriggered;
+    cefpTree->SetBranchAddress("fCefpSelected", &fCefpSelected);
+    cefpTree->SetBranchAddress("fCefpTriggered", &fCefpTriggered);
+    for (int i = 0; i < cefpTree->GetEntries(); i++) {
+      cefpTree->GetEntry(i);
+      newFilters->Fill(0);
+      newScalers->Fill(0);
+      for (ULong64_t j = 0; j < 64; j++) {
+        if (fCefpSelected & (1ull << j)) newFilters->Fill(j + 1);
+        if (fCefpTriggered & (1ull << j)) newScalers->Fill(j + 1);
+      }
+      if (fCefpSelected) newFilters->Fill(newFilters->GetNbinsX() - 1);
+      if (fCefpTriggered) newScalers->Fill(newScalers->GetNbinsX() - 1);
+    }
+  }
+
+  outputFile.cd();
+  newFilters->Write();
+  newScalers->Write();
+  newScalers->Divide(refScalers);
+  newFilters->Divide(refFilters);
+  newFilters->Write("ratioFilters");
+  newScalers->Write("ratioScalers");
+}
