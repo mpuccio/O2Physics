ef29ff15f84bf17e4238ccda7ec6b65ab1e0faa8
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2025-04-03 19:34:15 +0200

    Revert "[PWGJE,EMCAL-670] Bugfix in EMCalCorrectionTask for ShaperCorrection (#8883)"
    This reverts commit 1a597d497d79275819c01d2d9e204ecc505e2e11.

--- PWGJE/TableProducer/emcalCorrectionTask.cxx
+++ PWGJE/TableProducer/emcalCorrectionTask.cxx
@@ -288,7 +288,7 @@ struct EmcalCorrectionTask {
       std::vector<int64_t> cellIndicesBC;
       for (const auto& cell : cellsInBC) {
         auto amplitude = cell.amplitude();
-        if (static_cast<bool>(hasShaperCorrection) && emcal::intToChannelType(cell.cellType()) == emcal::ChannelType_t::LOW_GAIN) { // Apply shaper correction to LG cells
+        if (static_cast<bool>(hasShaperCorrection)) {
           amplitude = o2::emcal::NonlinearityHandler::evaluateShaperCorrectionCellEnergy(amplitude);
         }
         if (applyCellAbsScale) {
@@ -419,7 +419,7 @@ struct EmcalCorrectionTask {
           mHistManager.fill(HIST("hMCParticleEnergy"), cellparticle.e());
         }
         auto amplitude = cell.amplitude();
-        if (static_cast<bool>(hasShaperCorrection) && emcal::intToChannelType(cell.cellType()) == emcal::ChannelType_t::LOW_GAIN) { // Apply shaper correction to LG cells
+        if (static_cast<bool>(hasShaperCorrection)) {
           amplitude = o2::emcal::NonlinearityHandler::evaluateShaperCorrectionCellEnergy(amplitude);
         }
         cellsBC.emplace_back(cell.cellNumber(),
